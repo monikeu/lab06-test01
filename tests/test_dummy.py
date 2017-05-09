a01d82cb3f9f9e6ec638d3b72ee36d0cfd31854a
Monika Darosz <monikeu@users.noreply.github.com>
2017-05-09 16:57:59 +0200

    test

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -16,6 +16,30 @@ class TestDummy(unittest.TestCase):
         retcode = main(("--year", "2001", "--month", "1", "--day", "3"))
         self.assertEqual(retcode, 0)
 
+    def test_date(self):
+        weekday = calculate(-1, 1, 3)
+        self.assertEqual(weekday, -1)
+
+        weekday = calculate(1, 40, 3)
+        self.assertEqual(weekday, -1)
+
+        weekday = calculate(1, 1, 40)
+        self.assertEqual(weekday, -1)
+
+    def test_input(self):
+
+        retcode = main(("--year", "hehe", "--month", "1", "--day", "3"))
+        self.assertEqual(retcode, 1)
+
+        retcode = main(("--year", "2001", "--month", "fdvbds", "--day", "3"))
+        self.assertEqual(retcode, 1)
+
+        retcode = main(("--year", "2001", "--month", "1", "--day", "dvsdv"))
+        self.assertEqual(retcode, 1)
+
+        retcode = main("dffsfsa")
+        self.assertEqual(retcode, 1)
+        
 
 if __name__ == '__main__':
     unittest.main()
